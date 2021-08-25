2fb78c7119167b562e63bdf15a83f8227030b0ff
Davide Ornaghi <turtlearm97@gmail.com>
2021-08-26 00:03:30 +0200

    Correctly reset formatted variable before reusing it

--- keybag_parser.py
+++ keybag_parser.py
@@ -14,7 +14,6 @@ with open(filename, "rb") as f:
         exit(-1)
     salt = data[saltposition + 8: saltposition + 28]
     formatted = ""
-    formattedpub = ""
     for byte in salt:
         formatted += hex(byte).replace("0x", "\\x")
     print("Salt: {}".format(formatted))
@@ -39,17 +38,19 @@ with open(filename, "rb") as f:
             formatted += hex(byte).replace("0x", "\\x")
         print("Wrapped class {} key: {}".format(sys.argv[2], formatted))
     else:
+        formatted = ""
+        formattedpub = ""
         position = data.find(b"\x50\x42\x4B\x59")
         if position < 0:
             print("Public key not found")
             exit(-1)
         print("Position: ", position)
         pubkey = data[position + 8: position + 40]
-        position = data.find(b"\x43\x4C\x41\x53\x00\x00\x00\x04\x00\x00\x00" + dpclass) # wrong for some reason
+        position = data.find(b"\x43\x4C\x41\x53\x00\x00\x00\x04\x00\x00\x00" + dpclass)
         if position < 0:
             print("Private key not found")
             exit(-1)
-        privkey = data[position + 36: position + 76]
+        privkey = data[position + 44: position + 84]
         print("Position: ", position)
         for byte in pubkey:
             formattedpub += hex(byte).replace("0x", "\\x")
